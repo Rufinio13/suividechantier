388846460d3dc49a9c31e6860a888c4ca515c6ff
Rufinio13 <reavriste@maisonsnaturea.fr>
2026-01-31 17:54:40 +0100

    modif authoprovider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -26,7 +26,7 @@ export function AuthProvider({ children }) {
     try {
       // Timeout Promise
       const timeoutPromise = new Promise((_, reject) => {
-        setTimeout(() => reject(new Error('Timeout')), 10000);
+        setTimeout(() => reject(new Error('Timeout')), 100000);
       });
       
       // Requête Supabase
